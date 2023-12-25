85a8e3f9317622a62738278716fdcf0381daa6c0
MihPar <9496260@gmail.com>
2023-12-25 20:05:50 +0300

    fix2312252005

--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,8 +1,4 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { UserClass, UserSchema } from 'src/schema/user.schema';
-// import { UsersController } from 'src/api/users/users.controller';
-// import { UsersQueryRepository } from 'src/api/users/users.queryRepository';
+
 // import { UserService } from 'src/api/users/user.service';
 // import { UsersRepository } from 'src/api/users/user.repository';
 // import { EmailManager } from 'src/api/manager/email.manager';
