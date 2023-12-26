c9efeca65f0280243eeb2714cd931b69e5c820c7
MihPar <9496260@gmail.com>
2023-12-26 17:16:33 +0300

    fix2312261716

--- src/api/jwt/jwt.service.ts
+++ src/api/jwt/jwt.service.ts
@@ -1,42 +1,42 @@
-import { Injectable } from "@nestjs/common"
-import jwt from "jsonwebtoken"
-import { Users } from "../users/user.class"
-import { ObjectId } from "mongodb"
+// import { Injectable } from "@nestjs/common"
+// import jwt from "jsonwebtoken"
+// import { Users } from "../users/user.class"
+// import { ObjectId } from "mongodb"
 
-@Injectable()
-export class JWTService {
-	constructor() {}
-	async decodeRefreshToken(refreshToken: string) {
-		try {
-			const result = jwt.decode(refreshToken)
-			return result as jwt.JwtPayload
-	   } catch(err) {
-		   return null
-	   }
-	}
+// @Injectable()
+// export class JWTService {
+// 	constructor() {}
+// 	async decodeRefreshToken(refreshToken: string) {
+// 		try {
+// 			const result = jwt.decode(refreshToken)
+// 			return result as jwt.JwtPayload
+// 	   } catch(err) {
+// 		   return null
+// 	   }
+// 	}
 
-	async createJWT(user: Users) {
-		const token: string = await jwt.sign({userId: user._id}, process.env.JWT_SECRET!, {expiresIn: '5m'})
-		return token
-	}
+// 	async createJWT(user: Users) {
+// 		const token: string = await jwt.sign({userId: user._id}, process.env.JWT_SECRET!, {expiresIn: '5m'})
+// 		return token
+// 	}
 
-	async createRefreshJWT(userId: string, existDeviceId?: ObjectId) {
-		const deviceId: ObjectId = new ObjectId()
-		const refreshToken: string = await jwt.sign({deviceId: existDeviceId ?? deviceId, userId}, process.env.REFRESH_JWT_SECRET as string, {expiresIn: '10m'})
-		return refreshToken
-	}
+// 	async createRefreshJWT(userId: string, existDeviceId?: ObjectId) {
+// 		const deviceId: ObjectId = new ObjectId()
+// 		const refreshToken: string = await jwt.sign({deviceId: existDeviceId ?? deviceId, userId}, process.env.REFRESH_JWT_SECRET as string, {expiresIn: '10m'})
+// 		return refreshToken
+// 	}
 
-	getLastActiveDate(token: string) {
-		const result: any = jwt.decode(token)
-		return new Date(result.iat * 1000).toISOString()
-	}
+// 	getLastActiveDate(token: string) {
+// 		const result: any = jwt.decode(token)
+// 		return new Date(result.iat * 1000).toISOString()
+// 	}
 
-	async getUserIdByToken(token: string) {
-		try {
-			const result: any = await jwt.verify(token, process.env.JWT_SECRET!)
-			return new ObjectId(result.userId)
-		} catch(err) {
-			return null
-		}
-	}
-}
+// 	async getUserIdByToken(token: string) {
+// 		try {
+// 			const result: any = await jwt.verify(token, process.env.JWT_SECRET!)
+// 			return new ObjectId(result.userId)
+// 		} catch(err) {
+// 			return null
+// 		}
+// 	}
+// }
