6f43e55f23eda097fc690a7a91a9b0b5d0b12cc0
MihPar <9496260@gmail.com>
2023-12-27 15:51:31 +0300

    fix2312271551

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
+const PORT = process.env.PORT || 5000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
