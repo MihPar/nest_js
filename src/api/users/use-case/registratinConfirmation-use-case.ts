463df6e06e86a9cde370d5e84f0c9bd0788a81c5
MihPar <9496260@gmail.com>
2024-01-17 00:22:57 +0300

    fix2401170022

--- src/api/users/use-case/registratinConfirmation-use-case.ts
+++ src/api/users/use-case/registratinConfirmation-use-case.ts
@@ -1,25 +1,25 @@
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs"
-import { InputDateReqConfirmClass } from "../../auth/auth.class"
-import { UsersQueryRepository } from "api/users/users.queryRepository";
-import { UsersRepository } from "api/users/user.repository";
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { InputDateReqConfirmClass } from '../../auth/auth.class';
+import { UsersQueryRepository } from 'api/users/users.queryRepository';
+import { UsersRepository } from 'api/users/user.repository';
 
 export class RegistrationConfirmation {
-	constructor(
-		public inputDateRegConfirm: InputDateReqConfirmClass
-	) {}
+  constructor(public inputDateRegConfirm: InputDateReqConfirmClass) {}
 }
 
 @CommandHandler(RegistrationConfirmation)
-export class RegistrationConfirmationCase implements ICommandHandler<RegistrationConfirmation> {
-	constructor(
-		protected readonly usersQueryRepository: UsersQueryRepository,
-		protected readonly usersRepository: UsersRepository
-	) {}
-	async execute(command: RegistrationConfirmation) {
-		const user = await this.usersQueryRepository.findUserByConfirmation(command.inputDateRegConfirm.code);
-		const result = await this.usersRepository.updateConfirmation(user!._id);
-		return result;
-	}
+export class RegistrationConfirmationCase
+  implements ICommandHandler<RegistrationConfirmation>
+{
+  constructor(
+    protected readonly usersQueryRepository: UsersQueryRepository,
+    protected readonly usersRepository: UsersRepository,
+  ) {}
+  async execute(command: RegistrationConfirmation) {
+    const user = await this.usersQueryRepository.findUserByConfirmation(
+      command.inputDateRegConfirm.code,
+    );
+    const result = await this.usersRepository.updateConfirmation(user!._id);
+    return result;
+  }
 }
-
-
