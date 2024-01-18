632b8e4a85883842af895770f801a59b2f68ed36
MihPar <9496260@gmail.com>
2024-01-18 20:39:50 +0300

    fix2401192039

--- src/api/comment/use-case/updateLikeStatus-use-case.ts
+++ src/api/comment/use-case/updateLikeStatus-use-case.ts
@@ -8,7 +8,7 @@ export class UpdateLikestatus {
 	constructor(
 		public status: InputModelLikeStatusClass,
 		public id: inputModelCommentId,
-		public userId: ObjectId
+		public userId: string
 	) {}
 }
 
@@ -20,33 +20,33 @@ export class UpdateLikestatusCase implements ICommandHandler<UpdateLikestatus> {
 
 	) {}
 	async execute(command: UpdateLikestatus): Promise<boolean> {
-		const findLike = await this.likesRepository.findLikeCommentByUser(command.id.commentId, new ObjectId(command.userId))
+		const findLike = await this.likesRepository.findLikeCommentByUser(command.id.commentId, command.userId)
 	if(!findLike) {
-		await this.likesRepository.saveLikeForComment(command.id.commentId, new ObjectId(command.userId), command.status.likeStatus)
+		await this.likesRepository.saveLikeForComment(command.id.commentId, command.userId, command.status.likeStatus)
 		const resultCheckListOrDislike = await this.commentRepositoriy.increase(command.id.commentId, command.status.likeStatus)
 		return true
 	} 
 	
 	if((findLike.myStatus === 'Dislike' || findLike.myStatus === 'Like') && command.status.likeStatus === 'None'){
-		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, new ObjectId(command.userId), command.status.likeStatus)
+		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, command.userId, command.status.likeStatus)
 		const resultCheckListOrDislike = await this.commentRepositoriy.decrease(command.id.commentId, findLike.myStatus)
 		return true
 	}
 
 	if(findLike.myStatus === 'None' && (command.status.likeStatus === 'Dislike' || command.status.likeStatus === 'Like')) {
-		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, new ObjectId(command.userId), command.status.likeStatus)
+		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, command.userId, command.status.likeStatus)
 		const resultCheckListOrDislike = await this.commentRepositoriy.increase(command.id.commentId, command.status.likeStatus)
 		return true
 	}
 
 	if(findLike.myStatus === 'Dislike' && command.status.likeStatus === 'Like') {
-		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, new ObjectId(command.userId), command.status.likeStatus)
+		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, command.userId, command.status.likeStatus)
 		const changeDislikeOnLike = await this.commentRepositoriy.increase(command.id.commentId, command.status.likeStatus)
 		const changeLikeOnDislike = await this.commentRepositoriy.decrease(command.id.commentId, findLike.myStatus)
 		return true
 	}
 	if(findLike.myStatus === 'Like' && command.status.likeStatus === 'Dislike') {
-		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, new ObjectId(command.userId), command.status.likeStatus)
+		await this.likesRepository.updateLikeStatusForComment(command.id.commentId, command.userId, command.status.likeStatus)
 		const changeLikeOnDislike = await this.commentRepositoriy.decrease(command.id.commentId, findLike.myStatus)
 		const changeDislikeOnLike = await this.commentRepositoriy.increase(command.id.commentId, command.status.likeStatus)
 		return true
