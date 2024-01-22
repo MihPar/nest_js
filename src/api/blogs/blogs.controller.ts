a10acc1cb726746f67f0d1d17dd8b3327314ad62
MihPar <9496260@gmail.com>
2024-01-22 17:21:06 +0300

    fix2401221721

--- src/api/blogs/blogs.controller.ts
+++ src/api/blogs/blogs.controller.ts
@@ -81,7 +81,7 @@ export class BlogsController {
       sortDirection: string;
     },
   ) {
-	if(!userId) return null
+	if(!userId) throw new NotFoundException("404")  //
     const blog = await this.blogsQueryRepository.findBlogById(dto.blogId);
     if (!blog) throw new NotFoundException('Blogs by id not found');
     const getPosts: PaginationType<Posts> =
