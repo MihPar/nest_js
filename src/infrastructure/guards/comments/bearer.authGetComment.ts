a68f62df10b33c9c10a78d1f0c7d817566c274fa
MihPar <9496260@gmail.com>
2024-01-22 23:09:59 +0300

    fix2401222309

--- src/infrastructure/guards/comments/bearer.authGetComment.ts
+++ src/infrastructure/guards/comments/bearer.authGetComment.ts
@@ -18,40 +18,40 @@ export class CheckRefreshTokenForGet implements CanActivate {
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req: Request = context.switchToHttp().getRequest();
-	// // console.log("req.headers.authorization: ", req.headers.authorization)
-    // // if (!req.headers.authorization) throw new UnauthorizedException('401');
-	// let payload
-	// let token = req.headers.authorization
-	// if (!token) {
-	// 		req.user = null 
-	// 		return true
-	// 	} try {
-	// 	payload = await this.jwtService.verifyAsync(token, {secret: process.env.JWT_SECRET!})
-	// 	const resultAuth = await this.usersQueryRepository.findUserById(payload.userId);
-	// 	req['user'] = resultAuth;
-    //     return true;
-	// } catch(error) {
-	// 	console.log(error)
-	// 	return true
-	// }
-	
-	let token
+	// console.log("req.headers.authorization: ", req.headers.authorization)
+    // if (!req.headers.authorization) throw new UnauthorizedException('401');
 	let payload
-	if (!req.headers.authorization) {
-		req.user = null
-	} else {
-		token = req.headers.authorization.split(' ')[1];
+	let token = req.headers.authorization
+	if (!token) {
+			req.user = null 
+			return true
+		} try {
 		payload = await this.jwtService.verifyAsync(token, {secret: process.env.JWT_SECRET!})
-	}
-    if (payload) {
-      const resultAuth = await this.usersQueryRepository.findUserById(payload.userId);
-      if (resultAuth) {
-        req['user'] = resultAuth;
+		const resultAuth = await this.usersQueryRepository.findUserById(payload.userId);
+		req['user'] = resultAuth;
         return true;
-      }
-      return true;
-    } else {
-      return true;
-    }
+	} catch(error) {
+		console.log(error)
+		return true
+	}
+	
+	// let token
+	// let payload
+	// if (!req.headers.authorization) {
+	// 	req.user = null
+	// } else {
+	// 	token = req.headers.authorization.split(' ')[1];
+	// 	payload = await this.jwtService.verifyAsync(token, {secret: process.env.JWT_SECRET!})
+	// }
+    // if (payload) {
+    //   const resultAuth = await this.usersQueryRepository.findUserById(payload.userId);
+    //   if (resultAuth) {
+    //     req['user'] = resultAuth;
+    //     return true;
+    //   }
+    //   return true;
+    // } else {
+    //   return true;
+    // }
   }
 }
