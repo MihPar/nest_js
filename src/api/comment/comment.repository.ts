ea423676f43f06f7c1514703e187b1bbbdca297d
MihPar <9496260@gmail.com>
2025-03-03 10:04:50 +0300

    to second applay linter

--- src/api/comment/comment.repository.ts
+++ src/api/comment/comment.repository.ts
@@ -1,60 +1,76 @@
-import { CommentClass, CommentDocument } from "../../schema/comment.schema";
-import { Model } from "mongoose";
-import { InjectModel } from "@nestjs/mongoose";
-import { ObjectId } from "mongodb";
+import { CommentClass, CommentDocument } from '../../schema/comment.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { ObjectId } from 'mongodb';
 
 export class CommentRepository {
-	constructor(
-		@InjectModel(CommentClass.name) private commentModel: Model<CommentDocument>
-	) {}
+  constructor(
+    @InjectModel(CommentClass.name)
+    private commentModel: Model<CommentDocument>,
+  ) {}
 
-	async deleteAllComments() {
-		const deletedAll = await this.commentModel.deleteMany({});
-		return deletedAll.deletedCount === 1;
-	}
+  async deleteAllComments() {
+    const deletedAll = await this.commentModel.deleteMany({});
+    return deletedAll.deletedCount === 1;
+  }
 
+  async increase(commentId: string, likeStatus: string) {
+    if (likeStatus !== 'Dislike' && likeStatus !== 'Like') {
+      return;
+    }
+    return await this.commentModel.updateOne(
+      { _id: new ObjectId(commentId) },
+      {
+        $inc:
+          likeStatus === 'Dislike' ? { dislikesCount: 1 } : { likesCount: 1 },
+      },
+    );
+  }
 
-	async increase(commentId: string, likeStatus: string){
-		if(likeStatus !== 'Dislike' && likeStatus !== 'Like') {
-			return
-		} 
-		return await this.commentModel.updateOne({_id: new ObjectId(commentId)}, {$inc: likeStatus === 'Dislike' ? {dislikesCount: 1} : {likesCount: 1}})
-	}
+  async decrease(commentId: string, likeStatus: string) {
+    if (likeStatus !== 'Dislike' && likeStatus !== 'Like') {
+      return;
+    }
+    return await this.commentModel.updateOne(
+      { _id: new ObjectId(commentId) },
+      {
+        $inc:
+          likeStatus === 'Dislike' ? { dislikesCount: -1 } : { likesCount: -1 },
+      },
+    );
+  }
 
-	async decrease(commentId: string, likeStatus: string){
-		if(likeStatus !== 'Dislike' && likeStatus !== 'Like') {
-			return
-		} 
-		return await this.commentModel.updateOne({_id: new ObjectId(commentId)}, {$inc: likeStatus === 'Dislike' ? {dislikesCount: -1} : {likesCount: -1}})
-	}
+  async updateComment(commentId: string, content: string) {
+    const updateOne = await this.commentModel.updateOne(
+      { _id: new ObjectId(commentId) },
+      { $set: { content: content } },
+    );
+    return updateOne.matchedCount === 1;
+  }
 
-	async updateComment(commentId: string, content: string) {
-		const updateOne = await this.commentModel.updateOne(
-		  { _id: new ObjectId(commentId) },
-		  { $set: { content: content } }
-		);
-		return updateOne.matchedCount === 1;
-	  }
+  async deleteComment(commentId: string): Promise<boolean> {
+    try {
+      const deleteComment = await this.commentModel
+        .deleteOne({
+          _id: new ObjectId(commentId),
+        })
+        .exec();
+      return deleteComment.deletedCount === 1;
+    } catch (err) {
+      return false;
+    }
+  }
 
-	  async deleteComment(commentId: string): Promise<boolean> {
-		try {
-		  const deleteComment = await this.commentModel.deleteOne({
-			_id: new ObjectId(commentId),
-		  }).exec()
-		  return deleteComment.deletedCount === 1;
-		} catch (err) {
-		  return false; 
-		}
-	  }
-
-	  async createNewCommentPostId(newComment: CommentClass): Promise<CommentClass | null> {
-		try {
-			const saveComment = await this.commentModel.create(newComment);
-			await saveComment.save()
-			return newComment
-		} catch (error) {
-			console.log(error, 'error in create post');
-			return null;
-		  }
-	  }
+  async createNewCommentPostId(
+    newComment: CommentClass,
+  ): Promise<CommentClass | null> {
+    try {
+      const saveComment = await this.commentModel.create(newComment);
+      await saveComment.save();
+      return newComment;
+    } catch (error) {
+      console.log(error, 'error in create post');
+      return null;
+    }
+  }
 }
